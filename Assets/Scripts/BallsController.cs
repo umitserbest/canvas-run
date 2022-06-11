7b227ca8e7e99f02b6780a5f4bbe7f18660c7bd4
Ümit Serbest <umit@deadlyarrow.com>
2022-06-11 16:47:51 +0300

    Basic Movements

--- Assets/Scripts/BallsController.cs
+++ Assets/Scripts/BallsController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BallsController : MonoBehaviour
@@ -5,7 +6,10 @@ public class BallsController : MonoBehaviour
     public static BallsController Instance { get; private set; }
     
     [SerializeField] private GameObject ballPrefab;
-    
+    [SerializeField] private Transform[] ballHeadPositions;
+
+    private List<List<GameObject>> _balls = new();
+
     private void Awake()
     {
         if (Instance == null)
@@ -14,15 +18,56 @@ public class BallsController : MonoBehaviour
         }
     }
 
-    public void CreateBalls(int length,int width)
+    private void Update()
+    {
+        MoveHeads();
+        MoveMinions();
+    }
+
+    private void MoveHeads()
+    {
+        foreach (var head in ballHeadPositions)
+        {
+            var newPos = head.position - Vector3.forward;
+            var followTime = Time.deltaTime * 1f;
+            head.SetPositionAndRotation(Vector3.Lerp(head.position, newPos, followTime), head.transform.rotation);
+        }
+    }
+
+    private void MoveMinions()
+    {
+        for (var i = 0; i < _balls.Count; i++)
+        {
+            for (var j = 0; j < _balls[i].Count; j++)
+            {
+                var head = i == 0 ? ballHeadPositions[j] : _balls[i - 1][j].transform;
+
+                var current = _balls[i][j];
+                var distance = Vector3.Distance(head.position, current.transform.position);
+                var target = head.position;
+                var time = Time.deltaTime * distance * 1f;
+
+                current.transform.SetPositionAndRotation(
+                    Vector3.Lerp(current.transform.position, target, time),
+                    Quaternion.Lerp(current.transform.rotation, head.rotation, time));
+            }
+        }
+    }
+
+    public void CreateBalls(int length, int width)
     {
         for (var i = 0; i < length; i++)
         {
+            var widthList = new List<GameObject>();
+            
             for (var j = 0; j < width; j++)
             {
                 var ball = Instantiate(ballPrefab, transform, false);
                 ball.transform.localPosition = new Vector3(0 - j, 0.5f, i);
+                widthList.Add(ball);
             }
+
+            _balls.Add(widthList);
         }
     }
 }
